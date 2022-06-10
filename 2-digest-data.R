f6fdd8dc66428224bd9e537ee1178724fa3c280e
Jindra Lacko <jindra.lacko@gmail.com>
2022-06-10 19:54:54 +0200

    digest funguje *opravdu*

--- 2-digest-data.R
+++ 2-digest-data.R
@@ -1,10 +1,11 @@
 # načte ze zipáků 1 soubor
 library(readr)
 library(tidyr)
+library(dplyr)
 
 zip_files <- fs::dir_ls("./src/", glob = "*.csv.zip") 
 
-for (soubor in local_files) {
+for (soubor in zip_files) {
    
    unzip(soubor, exdir = "./src")
    
@@ -23,7 +24,7 @@ for (soubor in csv_files) {
                      col_types = cols(id = col_integer(),
                                       x = col_double(),
                                       y = col_double(), 
-                                      mp = col_character(),
+                                      mp = col_logical(),
                                       state = col_character(),
                                       types = col_character())))
    
@@ -32,12 +33,12 @@ for (soubor in csv_files) {
 # one hot encoding typů zločinů
 vysledek <- vysledek %>%                
    mutate(id = row_number(),                 
-          value = 1) %>%                     
+          value = TRUE) %>%                     
    separate_rows(types) %>%           
    pivot_wider(names_from = types, 
                names_prefix = 'type_',
                values_from = value, 
-               values_fill = 0) %>% 
+               values_fill = FALSE) %>% 
    select(-id)
 
 saveRDS(vysledek, "./src/mapa.rds")
